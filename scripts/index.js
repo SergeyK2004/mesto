b6b6a561cc1fb7babe399036d9d153f12c78c8fa
Sergey Krivoruchko <sergeykrivoruchko@iMac-Sergey.local>
2020-11-06 03:27:25 +1100

    Добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,58 @@
-const popup = document.querySelector(".popup");
-const popupForm = document.querySelector(".popup__form");
-const popupCloseButton = document.querySelector(".popup__button-close");
+const popupProfile = document.querySelector(".popup_profile");
+const popupNewCard = document.querySelector(".popup_new-card");
+const popupFormProfile = document.querySelector(".popup__form_profile");
+const popupFormNewCard = document.querySelector(".popup__form_new-card");
+const popupCloseButton = document.querySelectorAll(".popup__button-close");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileName = document.querySelector(".profile__name");
 const profileSpec = document.querySelector(".profile__spec");
 const inputName = document.querySelector(".popup__input_type_name");
 const inputSpec = document.querySelector(".popup__input_type_spec");
+const inputTitle = document.querySelector(".popup__input_type_title");
+const inputLink = document.querySelector(".popup__input_type_link");
+const profileAddButton = document.querySelector(".profile__add-button");
+const cardList = document.querySelector(".elements__list");
 
-function showPopup() {
+
+function showPopupProfile() {
   inputName.value = profileName.textContent;
   inputSpec.value = profileSpec.textContent;
-  popup.classList.add("popup_opened");
+  popupProfile.classList.add("popup_opened");
+}
+function showPopupNewCard() {
+  popupNewCard.classList.add("popup_opened");
 }
 
-function closePopup() {
-  popup.classList.remove("popup_opened");
+function closePopup(evt) {
+  evt.currentTarget.parentElement.parentElement.classList.remove("popup_opened");
 }
 
-function submitPopup(evt) {
-  // убрал свою проверку на пустые поля.
-  // поставил реквизит обязательного поля в input 
-  // теперь можно красиво выделить текст, случайно нажать пробел и сохранить.
-  // чем это лучше я не знаю, но лучше никакой отсебятины, это я понял.
-
-  evt.preventDefault();
+function submitPopupProfile(evt) {
+ evt.preventDefault();
   profileName.textContent = inputName.value;
   profileSpec.textContent = inputSpec.value;
-  closePopup();
+  closePopup(evt);
+}
+function submitPopupNewCard(evt) {
+  evt.preventDefault();
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+// наполняем содержимым
+cardElement.querySelector('.element__image').src = inputLink.value;
+cardElement.querySelector('.element__text').textContent = inputTitle.value;
+
+// отображаем на странице
+  cardList.prepend(cardElement); 
+  inputLink.value = '';
+  inputTitle.value = '';
+  closePopup(evt);
 }
 
-profileEditButton.addEventListener("click", showPopup);
-popupCloseButton.addEventListener("click", closePopup);
-popupForm.addEventListener('submit', submitPopup);
+profileEditButton.addEventListener("click", showPopupProfile);
+profileAddButton.addEventListener("click", showPopupNewCard);
+popupCloseButton.forEach((item) => {
+  item.addEventListener("click", closePopup);
+});
+popupFormProfile.addEventListener('submit', submitPopupProfile);
+popupFormNewCard.addEventListener('submit', submitPopupNewCard);
